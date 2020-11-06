90454380204ec440cf781b746026179814ea019c
Fernando <fcastillocosme@gmail.com>
2020-11-06 11:40:45 -0600

    Change photos, active navigation

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+//HEADER ON MOBILE
+$(document).on('click', '.mobile-nav-toggle', function (e) {
+    $('body').toggleClass('mobile-nav-active');
+});
+
+$(document).click(function (e) {
+    var container = $(".mobile-nav-toggle");
+    if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($('body').hasClass('mobile-nav-active')) {
+            $('body').removeClass('mobile-nav-active');
+        }
+    }
+});
+
+// ACTIVE NAVIGATION
+var nav_sections = $('section');
+var main_nav = $('.nav-menu, .mobile-nav');
+
+$(window).on('scroll', function() {
+  var cur_pos = $(this).scrollTop() + 200;
+
+  nav_sections.each(function() {
+    var top = $(this).offset().top,
+      bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      if (cur_pos <= bottom) {
+        main_nav.find('li').removeClass('active');
+      }
+      main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+    }
+    if (cur_pos < 300) {
+      $(".nav-menu ul:first li:first").addClass('active');
+    }
+  });
+});
